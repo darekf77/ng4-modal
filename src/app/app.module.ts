aa1236ca0b7a5ae0a583ca84fb3c8e5f931ce184
Dariusz Filipiak <dariusz.filipiak@igt.com>
2022-03-07 06:39:01 +0100

    new version 13.0.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { ModalModule } from 'components';
 
 import { AppComponent } from './app.component';
 import { ModalDemoComponent } from './modal-demo.component';
 import { NestedModalDemoComponent } from './nested-modal-demo.component';
 import { PanelDemoComponent } from './panel-demo.component';
+import { ModalModule } from '../lib/modal/modal-module';
 ;
 @NgModule({
   declarations: [
